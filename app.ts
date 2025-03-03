009775ebbf475a0cf993f958563ed14d1157d125
lucaslng <saculgnuel1@gmail.com>
2025-03-03 08:16:46 -0500

    idk

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express = require("express");
+import { getSystemDetails } from "./backend/system";
 let app = express();
 const port = 3000;
 
 app.get('/', (_, res: express.Response) => {
  res.send('Hello, World! 🌍');
+ res.send(getSystemDetails());
 });
 
 app.listen(port, () => {
