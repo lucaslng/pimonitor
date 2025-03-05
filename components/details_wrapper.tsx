9e24a842eef4d4c296add3961664a388dde23cfc
lucaslng <saculgnuel1@gmail.com>
2025-03-04 22:46:48 -0500

    remove force dynamic

--- components/details_wrapper.tsx
+++ components/details_wrapper.tsx
@@ -1,8 +1,6 @@
 import { getSystemDetails } from "@/actions/details";
 import Details from "./details";
 
-export const dynamic = "force_dynamic";
-
 export default async function DetailsWrapper() {
 	// await new Promise(r => setTimeout(r, 3000));
 
